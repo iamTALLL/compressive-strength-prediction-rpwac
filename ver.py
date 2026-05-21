f58f61cb88441f310a0a76f4512b78b64b640f53
iamTALLL <hoachi1607@gmail.com>
2026-05-21 23:13:36 +0700

    fix: update model

--- ver.py
+++ ver.py
@@ -11,5 +11,3 @@ print(f"Joblib Version: {joblib.__version__}")
 print(f"Gunicorn Version: {gunicorn.__version__}")
 
 import joblib
-model = joblib.load('xgb_woa_best_model.pkl')
-print(model.feature_names_in_.tolist())
